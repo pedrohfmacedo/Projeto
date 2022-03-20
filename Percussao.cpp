a6c7a4966dfe3b274968ea557b8495035073c245
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2022-03-20 09:55:09 -0300

    Add files via upload

---
+++ Percussao.cpp
@@ -0,0 +1,13 @@
+#include "Percussao.h"
+
+Percussao::Percussao(string tipo de cadeia = " "){
+	this -> tipo = tipo;
+}
+
+void Percussao::set_tipo de cadeia(string tipo de cadeia){
+	this -> tipo = tipo;	
+}
+
+string Percussao::get_tipo de cadeia(void){
+	return tipo;
+}
