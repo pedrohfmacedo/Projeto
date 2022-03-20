16a37ec5f6442a917c7cb617dcb931e9e4624e07
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2022-03-20 09:55:30 -0300

    Rename Estoque (1).h to Estoque.h

--- Estoque.h
+++ Estoque.h
@@ -4,30 +4,30 @@ class Estoque
 {   // Definindo vectors
 	vector <Produto> Lista_Produto;
 
-	// Método para ler produto
+	// MÃ©todo para ler produto
 	void lerProduto;
 
 public:
-    // Método para inserir produto
+    // MÃ©todo para inserir produto
     bool inserirProduto(Produto);
 
-    // Método para remover produto
+    // MÃ©todo para remover produto
     bool removerProduto(long int);
 
-    // Métodos para pesquisar produto
+    // MÃ©todos para pesquisar produto
     bool pesquisarProduto(string) const;
     bool pesquisarProduto(long int) const;
 
-    // Método para salvar produto
+    // MÃ©todo para salvar produto
     bool salvarProduto();
 
 	bool set_SaidaEstoqueProduto(unsigned int, int);
 	bool set_EntradaEstoqueProduto(unsigned int, int);
 
-	// Método para imprimir produto
+	// MÃ©todo para imprimir produto
 	void imprimeProduto() const;
 
-	// Método para ordenar produto
+	// MÃ©todo para ordenar produto
     void ordenaProduto();
 
 	int get_indiceProduto(string) const;
@@ -38,12 +38,12 @@ public:
 TECNICAS DE PROGRAMACAO 
 PROFESSOR: MARCUS SALERNO
 GRUPO: David Emanuel Albuquerque dos Santos- Turma 02
-José Vanilson de Brito Júnior - Turma 01
+JosÃ© Vanilson de Brito JÃºnior - Turma 01
 Lameque Dias de Oliveira - Turma 01
-Pedro Henrique Fernandes Macêdo -  Turma 02
+Pedro Henrique Fernandes MacÃªdo -  Turma 02
 Felipe Faustino do Nascimento- Turma 02
-Monitora:Ravânia Luciano Martildes
+Monitora:RavÃ¢nia Luciano Martildes
 PROJETO: LOJA DE INSTRUMENTOS MUSIDOM 
-DESCRIÇÃO: O programa deve realizar diversas funções entre eles, o cadastro de 
+DESCRIÃ‡ÃƒO: O programa deve realizar diversas funÃ§Ãµes entre eles, o cadastro de 
 produto, de cliente, e vendedores, gerenciar o estoque da loja, realizar pedidos
  */
